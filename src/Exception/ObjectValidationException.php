87d1d4f2a29afa029a5e3a47fefad062826caaab
石場優也 <ishiba@infiniteloop.co.jp>
2015-03-16 09:34:51 +0000

    adding exception

---
+++ src/Exception/ObjectValidationException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Vol2223\PyaValidation\Exception;
+
+class ObjectValidationException extends ValidationException {}
---
+++ src/Exception/ValidationException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Vol2223\PyaValidation\Exception;
+
+class ValidationException extends \Exception {}
