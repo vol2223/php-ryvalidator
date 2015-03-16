836eb30560753823740758f7365f6dfb50a0ed47
yuya ishiba <volment2223+github@gmail.com>
2015-03-16 22:58:28 +0900

    add validator type define

---
+++ src/ValidatorType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Vol2223\PyaValidator;
+
+class ValidatorType
+{
+	const VALIDATOR_TYPE_STRING  = 'string';  // バリデーター String
+	const VALIDATOR_TYPE_INTEGER = 'integer'; // バリデーター Integer
+	const VALIDATOR_TYPE_OBJECT  = 'object';  // バリデーター Object
+	const VALIDATOR_TYPE_ARRAY   = 'array';   // バリデーター Array
+	const VALIDATOR_TYPE_ENUM    = 'enum';    // バリデーター Enum
+}
